138d6d269c1c011b5aa17d05399e38cb044acf7a
Xianwei Zhou <11911224@mail.sustech.edu.cn>
2026-03-05 23:39:58 +0800

    fix: use num_beams=1 to avoid beam search KV cache bug

--- pipeline/step2_matting.py
+++ pipeline/step2_matting.py
@@ -173,8 +173,7 @@ class MattingProcessor:
                 input_ids=inputs["input_ids"],
                 pixel_values=inputs["pixel_values"],
                 max_new_tokens=1024,
-                do_sample=False,
-                num_beams=3,
+                num_beams=1,
             )
 
         generated_text = self._florence_processor.batch_decode(
